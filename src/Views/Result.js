b9babc0c04305cce31246cff7126b79155b27f89
llsrrll96 <58140426+llsrrll96@users.noreply.github.com>
2021-05-04 09:53:12 +0900

    upgrade design

--- src/Views/Result.js
+++ src/Views/Result.js
@@ -3,13 +3,33 @@ import './Result.css'
 
 function Result(props)
 {
-    let sondId = props.location.state
+    let songId = props.location.state
     const songinfo={ //더미 데이터
-        title : "title",
-        artist : "artist",
-        album : "album",
-        genre : "genre",
-        lyric : "lyric"
+        title : "라일락",
+        artist : "아이유",
+        album : "IU 5th Album 'LILAC",
+        genre : "댄스",
+        lyric : `나리는 꽃가루에 눈이 따끔해 (아야)
+        \n눈물이 고여도 꾹 참을래
+        내 마음 한켠 비밀스런 오르골에 넣어두고서
+        영원히 되감을 순간이니까
+        
+        우리 둘의 마지막 페이지를 잘 부탁해
+        어느 작별이 이보다 완벽할까
+        나리는 꽃가루에 눈이 따끔해 (아야)
+        눈물이 고여도 꾹 참을래
+        내 마음 한켠 비밀스런 오르골에 넣어두고서
+        영원히 되감을 순간이니까
+        
+        우리 둘의 마지막 페이지를 잘 부탁해
+        어느 작별이 이보다 완벽할까
+        나리는 꽃가루에 눈이 따끔해 (아야)
+        눈물이 고여도 꾹 참을래
+        내 마음 한켠 비밀스런 오르골에 넣어두고서
+        영원히 되감을 순간이니까
+        
+        우리 둘의 마지막 페이지를 잘 부탁해
+        어느 작별이 이보다 완벽할까`
     }
     
     //값 없이 들어 왔을 경우
@@ -21,16 +41,20 @@ function Result(props)
     
     const displaySong =()=>{
         console.log("result song1: " +props.location.state)
-        console.log("sondId: "+ sondId)
+        console.log("sondId: "+ songId)
         return (
-            <div className = "songinfo_side">
-                <div className ="songinfo">
-                    <p>{songinfo.title}</p>
-                    <p>{songinfo.artist}</p>
-                    <p>{songinfo.album}</p>
-                    <p>{songinfo.genre}</p>
-                    <p>{songinfo.lyric}</p>
+            <div>
+
+                <div className = "songinfo-border">
+                    <div className ="songinfo">
+                        <h5>{songinfo.title}</h5>
+                        <p>{songinfo.artist}</p>
+                        <p>{songinfo.album}</p>
+                        <p>{songinfo.genre}</p>
+                        <p>{songinfo.lyric}</p>
+                    </div>
                 </div>
+
             </div>
         )
     }
@@ -38,7 +62,6 @@ function Result(props)
     if (props.location.state){
         return (
             <div>
-                <span>{props.location.state}</span>
                 {displaySong()}
             </div>
         )
